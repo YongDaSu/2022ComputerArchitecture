507030d18a4297233816b0a7918b7b50ff5aaa24
YongDaSu <95763575+YongDaSu@users.noreply.github.com>
2022-11-29 21:26:50 +0800

    Add files via upload

---
+++ Lab03/HW3-2.c
@@ -0,0 +1,42 @@
+/* Lab01 c code
+   Leetcode 58. Length of Last Word
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLastWord(char *s){
+	int j = strlen(s)-1;
+	int length = 0;
+	
+	while(s[j] == ' '){
+		j--;
+	}
+	while(s[j] != ' ' && j>=0){
+		j--;
+		length++;
+	}
+	printf("length of last word is %d.", length);
+	printf("\n");
+}
+
+int main(){
+	char *str1 = "Hello World";
+	char *str2 = "i am a student  ";
+	char *str3 = "a";
+	lengthOfLastWord(str1);
+	lengthOfLastWord(str2);
+	lengthOfLastWord(str3);
+	
+	return 0;
+}
+
+/***terminal output
+length of last word is 5.
+length of last word is 7.
+length of last word is 1.
+
+--------------------------------
+Process exited after 0.02883 seconds with return value 0
+Press any key to continue . . .
+***/
